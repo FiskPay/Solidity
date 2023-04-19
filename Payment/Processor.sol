a38aed2fbf51cd936c52ecef82b7712395f1ab5f
Scrabakos <scrab4ever@hotmail.com>
2023-04-19 18:27:20 +0300

    cc

--- Payment/Processor.sol
+++ Payment/Processor.sol
@@ -105,7 +105,7 @@ contract Processor{
         if(size != 0)
             revert("Receiver is contract");
 
-        if(uint32(block.timestamp - 3 hours) > _timestamp)
+        if(uint32(block.timestamp - 5 minutes) > _timestamp)
             revert ("Transaction expired");
 
         ISubscribers sb = ISubscribers(pt.GetContractAddress(".Payment.Subscribers"));
